2051f8a268f72077ff4af4d46d780e82d64c6a1b
xi-xo <jose_aguiar@hotmail.co.uk>
2024-04-10 14:27:28 +0100

    ScrollView working. Working checkpoint

--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,20 +1,13 @@
-import React from "react"
-import { View, ScrollView } from "react-native"
-import Api from "../components/FetchEvents"
-import EventsList from "../components/EventsList"
-
+import React from "react";
+import { View } from "react-native";
+import Api from "../components/FetchEvents";
+import EventsList from "../components/EventsList";
 
 export default function HomePage() {
     return (
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <View>
-                <Api />
-            </View>
-            <ScrollView>
-                <View>
-                    <EventsList />
-                </View>
-            </ScrollView>
+        <View style={{ flex: 1 }}>
+            <Api />
+            <EventsList />
         </View>
-    )
+    );
 }
--- components/EventsList.js
+++ components/EventsList.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, StyleSheet, useWindowDimensions } from "react-native";
 import EventCard from "./EventCard";
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
 
 export default function EventsList({ events }) {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const window = useWindowDimensions();
 
     const handleEventPress = (event) => {
-        navigation.navigate("EventDetail", { event })
+        navigation.navigate("EventDetail", { event });
     };
 
+    const scrollViewHeight = events ? events.length * (window.height * 0.3) : 0;
+
     return (
-        <ScrollView>
-            <View>
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={[styles.scrollViewContent, { height: scrollViewHeight }]}>
                 {events && events.map(event => (
                     <EventCard key={event.id} event={event} onPress={() => handleEventPress(event)} />
                 ))}
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+    },
+});
