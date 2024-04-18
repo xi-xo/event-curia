d067bf253f3b21807b23ae32478a0222e2380912
xi-xo <jose_aguiar@hotmail.co.uk>
2024-04-18 13:33:17 +0100

    Resolves CustomHeader issue  in authentication page

--- components/navigation/CustomHeader.js
+++ components/navigation/CustomHeader.js
@@ -1,4 +1,4 @@
-//CustomHeader
+// CustomHeader.js
 import React, { useEffect, useState } from 'react';
 import { Appbar, Menu } from 'react-native-paper';
 import { useNavigation, useNavigationState } from '@react-navigation/native';
@@ -33,27 +33,23 @@ export default function CustomHeader({ title, isDark, user, userRole, onSignOut 
 
     console.log("User role:", userRole);
 
-    // Render only if user object and role exist
-    if (!user) {
-        return null;
-    }
-
     return (
         <Appbar.Header statusBarHeight={40} dark={isDark} style={{ backgroundColor: '#143D52' }}>
             {navigationState.routes.length > 1 && <Appbar.BackAction onPress={handleGoBack} />}
             <Appbar.Content title={title} titleStyle={{ alignSelf: 'center' }} />
-            
-            <Menu
-                visible={menuVisible}
-                onDismiss={closeMenu}
-                anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}
-            >
-                {userRole === 'staff' && (
-                    <Menu.Item onPress={navigateToPostEvent} title="Create Event" />
-                )}
-                <Menu.Item onPress={handleSignOut} title="Sign Out" />
-                {/* Add more Menu.Items for additional actions */}
-            </Menu>
+            {user && ( // Render the menu icon only if user is authenticated
+                <Menu
+                    visible={menuVisible}
+                    onDismiss={closeMenu}
+                    anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}
+                >
+                    {userRole === 'staff' && (
+                        <Menu.Item onPress={navigateToPostEvent} title="Create Event" />
+                    )}
+                    <Menu.Item onPress={handleSignOut} title="Sign Out" />
+                    {/* Add more Menu.Items for additional actions */}
+                </Menu>
+            )}
         </Appbar.Header>
     );
 }
