48b15c6e945b4c3bd060e01869e1f20ebcc3a606
xi-xo <jose_aguiar@hotmail.co.uk>
2024-04-18 14:33:32 +0100

    Adds about us page for both users, user and staff

--- components/navigation/CustomHeader.js
+++ components/navigation/CustomHeader.js
@@ -1,9 +1,8 @@
-// CustomHeader.js
 import React, { useEffect, useState } from 'react';
 import { Appbar, Menu } from 'react-native-paper';
 import { useNavigation, useNavigationState } from '@react-navigation/native';
 
-export default function CustomHeader({ title, isDark, user, userRole, onSignOut }) {
+export default function CustomHeader({ isDark, user, userRole, onSignOut }) {
     const [menuVisible, setMenuVisible] = useState(false);
     const navigation = useNavigation();
     const navigationState = useNavigationState(state => state);
@@ -27,16 +26,33 @@ export default function CustomHeader({ title, isDark, user, userRole, onSignOut 
     };
 
     const navigateToPostEvent = () => {
-        console.log("Navigating to PostEvent...");
-        navigation.navigate('PostEvent');
+        console.log("Navigating to CreateEvent...");
+        navigation.navigate('CreateEvent');
     };
+
+    const handleNavigateToAboutUs = () => {
+        navigation.navigate('AboutUs');
+    };
+
+    // Get the current screen name
+    const currentScreenName = navigationState.routes[navigationState.index].name;
+
+    let headerTitle = "Welcome"; // Default header title
+    if (currentScreenName === "HomePage") {
+        headerTitle = "Events"; // Set header title for HomePage
+    } else if (currentScreenName === "AboutUs") {
+        headerTitle = "About Us"; // Set header title for AboutUs page
+    } else if (currentScreenName === "CreateEvent") {
+        headerTitle = "Create event"; 
+    }
+    // Add more conditions for other screens as needed
 
     console.log("User role:", userRole);
 
     return (
         <Appbar.Header statusBarHeight={40} dark={isDark} style={{ backgroundColor: '#143D52' }}>
             {navigationState.routes.length > 1 && <Appbar.BackAction onPress={handleGoBack} />}
-            <Appbar.Content title={title} titleStyle={{ alignSelf: 'center' }} />
+            <Appbar.Content title={headerTitle} titleStyle={{ alignSelf: 'center' }} />
             {user && ( // Render the menu icon only if user is authenticated
                 <Menu
                     visible={menuVisible}
@@ -44,13 +60,12 @@ export default function CustomHeader({ title, isDark, user, userRole, onSignOut 
                     anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}
                 >
                     {userRole === 'staff' && (
-                        <Menu.Item onPress={navigateToPostEvent} title="Create Event" />
+                        <Menu.Item onPress={navigateToPostEvent} title="Create event" />
                     )}
+                    <Menu.Item onPress={handleNavigateToAboutUs} title="About us" />
                     <Menu.Item onPress={handleSignOut} title="Sign Out" />
-                    {/* Add more Menu.Items for additional actions */}
                 </Menu>
             )}
-        
         </Appbar.Header>
     );
 }
